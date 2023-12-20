6d7cb089134703cc05026a462514de34cdadc8d9
Troy <tvinsixiengmay@gmail.com>
2023-12-19 20:40:25 -0800

    Remove unneccessary variables

--- World/World.gd
+++ World/World.gd
@@ -5,9 +5,6 @@ extends Node2D
 func _ready():
 	$World_generator.connect("place_node", _on_place_node)
 
-func _exit_tree():
-	Global.world = null
-
 func _on_place_node(node, location):
 	#print("placing")
 	#print(global_position)
