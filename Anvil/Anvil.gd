7c1a2b201f20f0c4d0117049b64597b218ce2fc0
Troy <tvinsixiengmay@gmail.com>
2023-12-21 16:59:11 -0800

    Make anvil physics work with interacatables

--- Anvil/Anvil.gd
+++ Anvil/Anvil.gd
@@ -1,16 +1,28 @@
 extends CharacterBody2D
 
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var grabbed = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	if not is_on_floor() and not grabbed:
+		velocity.y += gravity * delta
+	if grabbed and not Input.is_key_pressed(KEY_B):
+		grabbed = false
+		global_position.x += 32
+		set_collision_layer_value(1, true)
+		set_collision_mask_value(1, true)
+	if not grabbed:
+		move_and_slide()
 
 func _on_player_try_to_carry_anvil(position):
+	grabbed = true
 	global_position = position
+	set_collision_layer_value(1, false)
+	set_collision_mask_value(1, false)
 
 func throw_anvil(): 
 	pass
