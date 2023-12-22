7d2667d814843a64193a6d6215f76d849026aa51
memes72 <slayr.x23@gmail.com>
2023-12-21 16:36:50 -0800

    changed player movement to a finite state machine

--- Player/player.gd
+++ Player/player.gd
@@ -35,56 +35,55 @@ func _physics_process(delta):
 	#Carry anvil
 	if Input.is_key_pressed(KEY_B):
 		try_to_carry_anvil.emit(global_position)
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_velocity
-
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	if direction:
-		velocity.x = direction * move_speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, move_speed)
-	if is_on_floor():
-		velocity.x = lerp(velocity.x, 0.0, 0.5 * delta)  # Adjust the second parameter for smoother deceleration
-
-	# Wavedash
-	if Input.is_action_just_pressed("ui_dash") and not is_dashing and is_on_floor():
-		print("WAVEDASHING !!!!!!!!!!!!!!!!!!")
-		is_wavedashing = true
-		wavedash_timer = wavedash_duration
-		# Reset velocity before wavedash
-		velocity.x = 0.0
-	
-	if is_wavedashing:
-		print("Wavedashing...")
-		# Gradually apply wavedash speed over wavedash duration
-		velocity.x = lerp(velocity.x, get_dash_direction() * wavedash_speed, delta / wavedash_duration)
-		wavedash_timer -= delta
-		if wavedash_timer <= 0.0:
-			is_wavedashing = false
-			# Reset velocity after wavedash completes
-			velocity.x = 0.0
-
-	# dash
-	if Input.is_action_just_pressed("ui_dash") and not is_dashing and not is_on_floor():
-		print("DASHING !!!!!!!!!!!!!!!!!!!!!")
-		is_dashing = true
-		dash_timer = dash_duration
-		# Reset velocity before dashing
-		velocity.x = 0.0
-		
-	if is_dashing:
-		print("Dashing...")
-		# Gradually apply dash speed over dash duration
-		velocity.x = lerp(velocity.x, get_dash_direction() * dash_speed, delta / dash_duration)
-		dash_timer -= delta
-		if dash_timer <= 0.0:
-			is_dashing = false
-			# Reset velocity after dash completes
-			velocity.x = 0.0
-
+	## Handle jump.
+	#if Input.is_action_just_pressed("jump") and is_on_floor():
+		#velocity.y = jump_velocity
+#
+#
+	## Get the input direction and handle the movement/deceleration.
+	## As good practice, you should replace UI actions with custom gameplay actions.
+	#var direction = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	#if direction:
+		#velocity.x = direction * move_speed
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, move_speed)
+	#if is_on_floor():
+		#velocity.x = lerp(velocity.x, 0.0, 0.5 * delta)  # Adjust the second parameter for smoother deceleration
+#
+	## Wavedash
+	#if Input.is_action_just_pressed("ui_dash") and not is_dashing and is_on_floor():
+		#print("WAVEDASHING !!!!!!!!!!!!!!!!!!")
+		#is_wavedashing = true
+		#wavedash_timer = wavedash_duration
+		## Reset velocity before wavedash
+		#velocity.x = 0.0
+	#
+	#if is_wavedashing:
+		#print("Wavedashing...")
+		## Gradually apply wavedash speed over wavedash duration
+		#velocity.x = lerp(velocity.x, get_dash_direction() * wavedash_speed, delta / wavedash_duration)
+		#wavedash_timer -= delta
+		#if wavedash_timer <= 0.0:
+			#is_wavedashing = false
+			## Reset velocity after wavedash completes
+			#velocity.x = 0.0
+#
+	## dash
+	#if Input.is_action_just_pressed("ui_dash") and not is_dashing and not is_on_floor():
+		#print("DASHING !!!!!!!!!!!!!!!!!!!!!")
+		#is_dashing = true
+		#dash_timer = dash_duration
+		## Reset velocity before dashing
+		#velocity.x = 0.0
+		#
+	#if is_dashing:
+		#print("Dashing...")
+		## Gradually apply dash speed over dash duration
+		#velocity.x = lerp(velocity.x, get_dash_direction() * dash_speed, delta / dash_duration)
+		#dash_timer -= delta
+		#if dash_timer <= 0.0:
+			#is_dashing = false
+			## Reset velocity after dash completes
+			#velocity.x = 0.0
 
 	move_and_slide()
---
+++ Scripts/npcIdle.gd
@@ -0,0 +1,27 @@
+extends State
+class_name npcIdle
+
+@export var npc: CharacterBody2D
+@export var move_speed := 25.0
+
+var move_direction : Vector2
+var wander_time : float
+
+func random_movement():
+	move_direction = Vector2(randf_range(-1,1), 0.0)
+	wander_time = randf_range(1, 3)
+	
+
+func Enter(): 
+	random_movement()
+
+func Update(delta: float):
+	if wander_time > 0:
+		wander_time -= delta
+	
+	else:
+		random_movement()
+
+func Physics_Update(delta: float):
+	if npc:
+		npc.velocity = move_direction * move_speed
