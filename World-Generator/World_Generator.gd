08eb6f872b284894e3cd626ddb7d71ec6605e41b
Troy <tvinsixiengmay@gmail.com>
2023-12-21 16:59:36 -0800

    Randomize object placement

--- World-Generator/World_Generator.gd
+++ World-Generator/World_Generator.gd
@@ -1,7 +1,7 @@
 extends Marker2D
 
 @export var block : PackedScene
-@export var object : PackedScene
+@export var objects : Array[PackedScene]
 
 @export var min_distance_between_objects : int
 @export var max_distance_between_objects : int
@@ -14,20 +14,17 @@ var rng;
 
 signal place_node(node, location)
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	rng = RandomNumberGenerator.new()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if (current_distance < max_world_distance):
 		global_position.x += grid_size
 		var block_instance = block.instantiate();
 		emit_signal("place_node", block_instance, global_position)
 		if distance_until_next_object <= 0:
-			#TODO randomize object to place
-			var object_instance = object.instantiate()
+			var random_index = rng.randi_range(0, objects.size()-1)
+			var object_instance = objects[random_index].instantiate()
 			distance_until_next_object = object_instance.width / grid_size 
 			distance_until_next_object += rng.randi_range(min_distance_between_objects, max_distance_between_objects)
 			emit_signal("place_node", object_instance, global_position)
